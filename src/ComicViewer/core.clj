9fdec8ca4341f4467e3bb2eea1a169972cfceaa9
애들아빠 <younwook@gmail.com>
2010-09-30 09:29:27 +0900

    상태변수 분리. 전역변수 참조 제거

--- src/ComicViewer/core.clj
+++ src/ComicViewer/core.clj
@@ -16,29 +16,72 @@
 (def image-resize-factor-down-threshold (atom 0.1))
 (def image-resize-factor (atom 1.0))
 
-(def current-zip-file-name (ref nil))
-(def current-zip-file (ref nil))
-(def current-page-list (ref nil))
-(def end-of-page (ref 0))
-(def current-position (ref 0))
-(def current-page-image (ref nil))
+(def image-updated? (atom false))
+(defn is-image-update-required? [] @image-updated?)
+(defn make-image-update-required [] (reset! image-updated? true))
+(defn make-image-update-completed [] (reset! image-updated? false))
+(defn change-image-update-status []
+  (if @image-updated?
+    (reset! image-updated? false)
+    (reset! image-updated? true))
+  (is-image-update-required?))
 
-(def image-updated? (ref false))
+(def image-cache (atom nil))
+(defn cache-image [model im]
+  (do
+    (println (format "cache-image : image : %s " im))
+    (reset! image-cache im)
+    (make-image-update-completed)))
+(defn get-cached-image [model] @image-cache)
+(defn init-image-cache [model]
+  (do
+    (println "image-cache initialized...")
+    (reset! image-cache nil)
+    (make-image-update-completed)))
+
+(def current-position (atom 0))
+(defn get-current-page [model]
+  @current-position)
+(defn go-next-page [model]
+  (if (= (get-current-page model) (model :end-of-page))
+    nil
+    (do
+      (reset! current-position (+ (get-current-page model) 1))
+      (make-image-update-required))))
+(defn go-prev-page [model]
+  (if (= (get-current-page model) 0)
+    nil
+    (do
+      (reset! current-position (- (get-current-page model) 1))
+      (make-image-update-required))))
+(defn go-start-page [model]
+  (do
+    (reset! current-position 0)
+    (make-image-update-required)))
+(defn go-end-page [model]
+  (do
+    (reset! current-position (model :end-of-page))
+    (make-image-update-required)))
+
+(defn init-global-environment []
+  (do
+    (go-start-page nil)
+    (init-image-cache nil)))
 
 (def resize-required? (ref true))
 (def current-file-directory (ref "."))
 
 (defn dec-resize-factor []
-  (dosync
+  (do
    (reset! image-resize-factor (max @image-resize-factor-down-threshold
 				    (- @image-resize-factor 0.05)))
-   (ref-set image-updated? true)))
+   (make-image-update-required)))
 
 (defn inc-resize-factor []
-  (dosync
+  (do
    (reset! image-resize-factor (min @image-resize-factor-up-threshold
 				    (+ @image-resize-factor 0.05)))
-   (ref-set image-updated? true)))
+   (make-image-update-required)))
 
 (def image-clip-x (atom 0))
 (def image-clip-y (atom 0))
@@ -46,7 +89,6 @@
 (def wheel-speed (atom 2))
 (defn get-wheel-speed []
   @wheel-speed)
-
 
 ;; Macros
 (defmacro with-JPanel [jpanel & body]
@@ -79,6 +121,7 @@
      (ref-set resize-required? v))))
 
 (defn resize-image [orig-image]
+  (println (format "resize-image : %s factor:%f" orig-image @image-resize-factor))
   (with-image orig-image
     (let [scaled-width (int (* image-width @image-resize-factor))
 	  scaled-height (int (* image-height @image-resize-factor))
@@ -92,38 +135,57 @@
 			 RenderingHints/VALUE_INTERPOLATION_BICUBIC)
       (.drawImage g image affine-xform nil)
       (.dispose g)
+      (println (format "resize-image : scaled image : %s" scaled-image))
       scaled-image)))
 	  
-(defn get-current-page-image []
-  (if @image-updated?
-    (dosync
-     (ref-set current-page-image
-	      (resize-image (ImageIO/read (.getInputStream @current-zip-file
-							   (nth @current-page-list @current-position)))))
-     (ref-set image-updated? false)))
-  @current-page-image)
+(defn get-current-page-image [model]
+  (println (format "total page:%d current page:%d"
+		   (count (model :current-page-list))
+		   (get-current-page model)))
+  (println (get-cached-image model))
+  (println (is-image-update-required?))
+  (if (is-image-update-required?)
+    (do
+      (println "get-current-page-image : image updating...")
+      (println "------------------------------------------")
+      (println (model :current-zip-file))
+      (println (get-current-page model))
+      (println (nth (model :current-page-list) 0))
+      (println (.getInputStream (model :current-zip-file)
+				(nth (model :current-page-list)
+				     (get-current-page model))))
+      (println (ImageIO/read
+		(.getInputStream (model :current-zip-file)
+				 (nth (model :current-page-list)
+				      (get-current-page model)))))
+      (cache-image model (resize-image
+			  (ImageIO/read
+			   (.getInputStream (model :current-zip-file)
+					    (nth (model :current-page-list)
+						 (get-current-page model))))))
+      (println (get-cached-image model))
+      (println "------------------------------------------")))
+  (get-cached-image model))
 
-
-(defn get-current-page-image-1 [panel]
-  (with-image (get-current-page-image)
+(defn get-current-page-image-1 [model panel]
+  (with-image (get-current-page-image model)
     (with-JPanel panel
-      (println (format "%d %d %d %d %d %d %d %d"
-		       @image-clip-x
-		       @image-clip-y
-		       image-width
-		       image-height
-		       (- (- image-width 1) @image-clip-x)
-		       (- (- image-height 1) @image-clip-y)
-		       panel-width
-		       panel-height))
+      ;; (println (format "%d %d %d %d %d %d %d %d"
+      ;; 		       @image-clip-x
+      ;; 		       @image-clip-y
+      ;; 		       image-width
+      ;; 		       image-height
+      ;; 		       (- (- image-width 1) @image-clip-x)
+      ;; 		       (- (- image-height 1) @image-clip-y)
+      ;; 		       panel-width
+      ;; 		       panel-height))
       (.getSubimage image
 		    @image-clip-x @image-clip-y
 		    (min panel-width (- (- image-width 1) @image-clip-x))
 		    (min panel-height (- (- image-height 1) @image-clip-y))))))
 
-
-(defn move-clip-area [page-panel x y]
-  (with-image (get-current-page-image)
+(defn move-clip-area [model page-panel x y]
+  (with-image (get-current-page-image model)
     (with-JPanel page-panel
       (dosync
        (let [new-clip-x (int (+ @image-clip-x (* x (get-wheel-speed))))
@@ -141,7 +203,18 @@
       (.getSubImage (.getScaledInstance im image-width image-height)
 		    x y w h))))
 
-(defn close-current-zip-file [])
+(defn close-current-zip-file [model]
+  (if (nil? (model :current-zip-file-name))
+    nil
+    (let [_ (go-start-page model)
+	  _ (init-image-cache model)
+	  _ (make-image-update-completed)
+	  _ (.close (model :current-zip-file))]
+      {:current-page-image nil
+       :end-of-page 0
+       :current-page-list nil
+       :current-zip-file nil
+       :current-zip-file-name nil})))
 
 (defn open-zip-file [filename]
   (let [zip-file (new ZipFile filename)
@@ -149,43 +222,15 @@
 			  (filter (fn [n] (not (nil?
 						(re-matches #".*[gif|png|jpg]$"
 							    (.getName n)))))
-				  (enumeration-seq (.getEntries zip-file))))]
-    (close-current-zip-file)
-    (dosync
-     (ref-set current-zip-file-name filename)
-     (ref-set current-zip-file zip-file)
-     (ref-set current-page-list page-seq)
-     (ref-set current-position 0)
-     (ref-set end-of-page (- (count page-seq) 1))
-     (ref-set current-page-image (get-current-page-image))
-     (ref-set image-updated? true))))
-
-(defn close-current-zip-file []
-  (if (nil? @current-zip-file-name)
-    nil
-    (dosync
-     (ref-set image-updated? false)
-     (ref-set current-page-image nil)
-     (ref-set end-of-page 0)
-     (ref-set current-position 0)
-     (ref-set current-page-list nil)
-     (.close @current-zip-file)
-     (ref-set current-zip-file nil)
-     (ref-set current-zip-file-name nil))))
-
-(defn go-next-page []
-  (if (= @current-position @end-of-page)
-    nil
-    (dosync
-     (ref-set current-position (+ @current-position 1))
-     (ref-set image-updated? true))))
-
-(defn go-prev-page []
-  (if (= @current-position 0)
-    nil
-    (dosync
-     (ref-set current-position (- @current-position 1))
-     (ref-set image-updated? true))))
+				  (enumeration-seq (.getEntries zip-file))))
+	_ (init-global-environment)
+	temp-model {:current-zip-file-name filename
+		    :current-zip-file zip-file
+		    :current-page-list page-seq
+		    :end-of-page (- (count page-seq) 1)
+		    :current-page-image nil}
+	_ (make-image-update-required)]
+    (assoc temp-model :current-page-image (get-current-page-image temp-model))))
 
 (defn setup-model []
   (println "model setup start"))
@@ -214,18 +259,19 @@
 ;; 만약 이 함수가 image 가 nil 일때 호출되면
 ;; 에러를 발생시키게 될 것이다.
 ;;
-(defn draw-info [g panel]
+(defn draw-info [g model panel]
   (let [orig-color (.getColor g)
 	orig-font (.getFont g)
 	panel-width (.getWidth panel)
 	panel-height (.getHeight panel)
-	image-width (.getWidth (get-current-page-image))
-	image-height (.getHeight (get-current-page-image))
-	image-resize-behavior (if @resize-required? "YES" "NO")]
+	image-width (.getWidth (get-current-page-image model))
+	image-height (.getHeight (get-current-page-image model))
+	image-resize-behavior (if @resize-required? "YES" "NO")
+	current-page (get-current-page model)]
     (draw-text g 0 0 Color/WHITE
 	       (format "page:%d pagefile:%s panel-width:%d panel-height:%d resize:%s clip-x:%d clip-y:%d resize-factor:%f"
-		       @current-position
-		       (.getName (nth @current-page-list @current-position))
+		       current-page
+		       (.getName (nth (model :current-page-list) current-page))
 		       (int panel-width)
 		       (int panel-height)
 		       image-resize-behavior
@@ -233,24 +279,26 @@
 		       (int @image-clip-y)
 		       @image-resize-factor))))
 
-(defn paint-page-panel-1 [g panel]
+(defn paint-page-panel-1 [g model panel]
   (with-JPanel panel
-    (with-image (get-current-page-image-1 panel)
+    (with-image (get-current-page-image-1 model panel)
       (let [resize-factor (get-resize-factor image-width
 					     image-height
 					     panel-width
 					     panel-height)]
-	(.drawImage g image 0 0
-;;		    (int (* image-width resize-factor))
-;;		    (int (* image-height resize-factor))
-		    panel)
-	(draw-info g panel)))))
+	(.drawImage g image 0 0	panel)
+	(draw-info g model panel))))
+  (draw-text g 40 40 Color/WHITE "TEST!!"))
 
-(defn create-image-panel []
+
+(defn create-image-panel [model]
+  (println "image-panel created")
   (proxy [JPanel ActionListener] []
     (paintComponent [g]
 		    (proxy-super paintComponent g)
-		    (paint-page-panel-1 g this))
+		    (paint-page-panel-1 g model this)
+		    (println (model :current-zip-file-name)))
+;;		    (println this))
     (actionPerformed [e]
 		     (.repaint this)
 		     )))
@@ -263,29 +311,37 @@
     (.showOpenDialog jfc nil) ;; 나중에 frame-window 와 연계 필요
     (dosync
      (ref-set current-file-directory (.getCurrentDirectory jfc)))
-    (open-zip-file (.getPath (.getSelectedFile jfc)))))
+    (println (.getPath (.getSelectedFile jfc)))
+    (.getPath (.getSelectedFile jfc))))
 
-(defn destroy-gui [comps]
+(defn destroy-gui [model comps]
   (.stop (:main-timer comps))
-  (.dispose (:frame-window comps))
-  (close-current-zip-file))
+  (.dispose (:frame-window comps)))
+;;  (close-current-zip-file model))
 
-(defn frame-window-key-listener [comps]
+(defn setup-gui [a])
+(defn frame-window-key-listener [model comps]
      (proxy [KeyListener] []
        (keyPressed [e]
 		   (let [key-code (.getKeyCode e)]
-		     (cond (= key-code KeyEvent/VK_LEFT) (go-prev-page)
-			   (= key-code KeyEvent/VK_RIGHT) (go-next-page)
+		     (cond (= key-code KeyEvent/VK_LEFT) (go-prev-page model)
+			   (= key-code KeyEvent/VK_RIGHT) (go-next-page model)
 			   (= key-code KeyEvent/VK_SLASH) (change-resize-behavior)
-			   (= key-code KeyEvent/VK_O) (open-file)
-			   (= key-code KeyEvent/VK_ESCAPE) (destroy-gui comps)
-			   (= key-code KeyEvent/VK_OPEN_BRACKET) (inc-resize-factor)
-			   (= key-code KeyEvent/VK_CLOSE_BRACKET) (dec-resize-factor)))
+			   ;; 새 파일 열기를 하면 아예 새로 창을 열고 현재 창을 닫는다.
+			   (= key-code KeyEvent/VK_O) (let [new-model (open-zip-file (open-file))]
+							(if (nil? new-model)
+							  nil
+							  (do
+							    (destroy-gui model comps)
+							    (setup-gui new-model))))
+			   (= key-code KeyEvent/VK_ESCAPE) (destroy-gui model comps)
+			   (= key-code KeyEvent/VK_OPEN_BRACKET) (dec-resize-factor)
+			   (= key-code KeyEvent/VK_CLOSE_BRACKET) (inc-resize-factor)))
 		   (println (format "key %d pressed!" (.getKeyCode e))))
        (keyTyped [e])
        (keyReleased [e])))
 
-(defn frame-window-wheel-listener [comps]
+(defn frame-window-wheel-listener [model comps]
      (proxy [MouseWheelListener] []
        (mouseWheelMoved [e]
 			(println (.paramString e))
@@ -294,46 +350,45 @@
 			      paramString (.paramString e)]
 			  (if (and (= mod 0)
 				   (= mod-ex 0))
-			    (move-clip-area (:page-panel comps)
+			    (move-clip-area model
+					    (:page-panel comps)
 					    0 (.getUnitsToScroll e))
 			    (if (and (= mod 1)
 				     (= mod-ex 64))
-			      (move-clip-area (:page-panel comps)
+			      (move-clip-area model
+					      (:page-panel comps)
 					      (.getUnitsToScroll e) 0)))))))
 
-(defn setup-actions [comps]
-  (.addKeyListener (:frame-window comps)
-		   (frame-window-key-listener comps))
-  (.addMouseWheelListener (:frame-window comps)
-			  (frame-window-wheel-listener comps)))
-
-(defn initialize-gui [comps]
+(defn setup-actions [model comps]
   (.addActionListener (:main-timer comps) (:page-panel comps))
+  (.addKeyListener (:frame-window comps)
+		   (frame-window-key-listener model comps))
+  (.addMouseWheelListener (:frame-window comps)
+			  (frame-window-wheel-listener model comps)))
+
+(defn initialize-gui [model comps]
   (.setSize (:frame-window comps) 500 500)
   (.setSize (:page-panel comps) 500 500)
   (.add (:frame-window comps) (:page-panel comps))
-  (setup-actions comps)
+  (setup-actions model comps)
   (.requestFocus (:frame-window comps))
   (.start (:main-timer comps)))
 
-(defn create-gui-components []
+(defn create-gui-components [model]
+  (println "new gui components created")
   {:main-timer (Timer. 100 nil)
    :frame-window (JFrame. "ComicViewer")
-   :page-panel (create-image-panel)})
+   :page-panel (create-image-panel model)})
 
-(defn setup-gui []
-  (let [gui-components (create-gui-components)
-	destroy-gui (fn []
-		      (.stop (:main-timer gui-components))
-		      (.dispose (:frame-window gui-components))
-		      (close-current-zip-file))
-	]
-    (initialize-gui gui-components)
-    (.setVisible (:frame-window gui-components) true)))
+(defn setup-gui [model]
+  (let [gui-components (create-gui-components model)]
+    (initialize-gui model gui-components)
+    (.setVisible (:frame-window gui-components) true)
+    (make-image-update-required)))
 
 (defn setup-application []
-  (setup-model)
-  (setup-gui))
+  (let [model (open-zip-file (open-file))]
+    (setup-gui model)))
 
 (defn start-appl []
   (setup-application))
